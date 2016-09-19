7c91c7ee8c37249f65a60b1c4f78afac85ed5a5a
Chad Martin <cmartin584@students.mchenry.edu>
2016-09-18 21:13:43 -0500

    Add files via upload

---
+++ Who Called for Bananas.rtf.py
@@ -0,0 +1,49 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1404\cocoasubrtf470
+{\fonttbl\f0\fnil\fcharset0 Menlo-Bold;\f1\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue109;\red15\green112\blue3;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx560\tx1120\tx1680\tx2240\tx2800\tx3360\tx3920\tx4480\tx5040\tx5600\tx6160\tx6720\pardirnatural\partightenfactor0
+
+\f0\b\fs28 \cf2 def 
+\f1\b0 \cf0 bananas():\uc0\u8232     
+\f0\b \cf2 print
+\f1\b0 \cf0 (
+\f0\b \cf3 "Being Bananas now is all that counts, this is WHO I am"
+\f1\b0 \cf0 )\uc0\u8232     
+\f0\b \cf2 print
+\f1\b0 \cf0 (
+\f0\b \cf3 "The Dr. says; Always take bananas to a party""\cf2 \\n\cf3 """
+\f1\b0 \cf0 )\uc0\u8232     
+\f0\b \cf2 print 
+\f1\b0 \cf0 (
+\f0\b \cf3 "Bananas are good, eat Bananas!"
+\f1\b0 \cf0 )\uc0\u8232     
+\f0\b \cf2 print 
+\f1\b0 \cf0 (
+\f0\b \cf3 "Monkeys eat Bananas, eat Bananas!"
+\f1\b0 \cf0 )\uc0\u8232     
+\f0\b \cf2 print 
+\f1\b0 \cf0 (
+\f0\b \cf3 "Bananas are good, 'WHO eats Banans?"
+\f1\b0 \cf0 )\uc0\u8232     
+\f0\b \cf2 print 
+\f1\b0 \cf0 (
+\f0\b \cf3 "Tall kids, short kids, little kids, fat kids,"
+\f1\b0 \cf0 )\uc0\u8232     
+\f0\b \cf2 print 
+\f1\b0 \cf0 (
+\f0\b \cf3 "skinny kids, big kids, all kids eat Bananas!"
+\f1\b0 \cf0 )\uc0\u8232     
+\f0\b \cf2 print 
+\f1\b0 \cf0 (
+\f0\b \cf3 "WHO'd  you say Doctor, eats Bananas?"
+\f1\b0 \cf0 )\uc0\u8232     
+\f0\b \cf2 print 
+\f1\b0 \cf0 (
+\f0\b \cf3 "Bananas are good""\cf2 \\n\cf3 "
+\f1\b0 \cf0 )\uc0\u8232     
+\f0\b \cf2 print 
+\f1\b0 \cf0 (
+\f0\b \cf3 "I eat Bananas!!!"
+\f1\b0 \cf0 )\uc0\u8232 \u8232 bananas( )\u8232 bananas( )\u8232 bananas( )\
+}
